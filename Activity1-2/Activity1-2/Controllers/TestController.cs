eed5c7663f7e903d783aa269c214d22895952634
Brian Cantrell <bribri546@gmail.com>
2020-11-08 14:28:03 -0700

    CST-247 Activity 1-2

---
+++ Activity1-2/Activity1-2/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1_2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public String Index()
+        {
+            return @"<b>Hello World as a string from Index</b>";
+        }
+        //GET: /Test/TestView
+        public ActionResult TestView()
+        {
+            return View();
+        }
+
+        //GET: /Test/ErrorMessage
+        public ActionResult ErrorMessage()
+        {
+            return View();
+        }  
+
+        public String Play()
+        {
+            return "<b>Hello World as a string from Play</b>";
+        }
+    }
+}
