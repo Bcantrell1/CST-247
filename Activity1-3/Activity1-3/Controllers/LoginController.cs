071a4ed855f4e080f0fadbdfb6cb9e2e39cd722d
Brian Cantrell <bribri546@gmail.com>
2020-11-08 14:15:58 -0700

    CST-247 Activity 1-3

---
+++ Activity1-3/Activity1-3/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Activity1_3.Models;
+using Activity1_3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1_3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel user)
+        {
+            SecurityService ss = new SecurityService();
+            Boolean success = ss.Authenticate(user);
+
+            if (success)
+            {
+                return View("LoginPassed", user);
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
