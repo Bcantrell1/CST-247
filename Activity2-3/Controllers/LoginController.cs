0bec3f3ce9703dbfd7c3fb705ac6877f4183e9b8
Brian Cantrell <bribri546@gmail.com>
2020-11-22 13:48:12 -0700

    Activity 2 Part 3

---
+++ Activity2-3/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Activity1_3.Models;
+using Activity1_3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1_3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        
+        [HttpPost]
+        public ActionResult Login(UserModel user)
+        {
+            if (!ModelState.IsValid) return View("Login");
+            SecurityService ss = new SecurityService();
+            Boolean success = ss.Authenticate(user);
+
+            if (success)
+            {
+                return View("LoginPassed", user);
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
---
+++ Activity2-3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,42 @@
+﻿using Activity1_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Activity1_3.Services.Data
+{
+    public class SecurityDAO
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public bool FindByUser(UserModel user)
+        {
+            bool success = false;
+
+            string queryString = "select * from dbo.Users where username = @USERNAME and password = @PASSWORD";
+
+            using(SqlConnection connect = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connect);
+                command.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 20).Value = user.Username;
+                command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar, 20).Value = user.Password;
+
+                try
+                {
+                    connect.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                        success = true;
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return success;
+        }
+    }
+}
